775e15fb1aa95fc197a5a5ffef90ae0e00bd352a
Will Hargrave willhargrave@hotmail.co.uk <willhargrave@hotmail.co.uk>
2022-11-14 19:49:58 +0900

    seeds (need fixing)

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,7 +2,8 @@ class Animal < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  # validates :price, presence: true
-  # validates :species, presence: true
-  # validates :name, uniqueness: true, presence: true
+  validates :price, presence: true
+  validates :species, presence: true
+  validates :name, uniqueness: true, presence: true
+  
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,11 @@
 class Booking < ApplicationRecord
+  enum status: [:accepted, :rejected, :pending]
   belongs_to :user
   belongs_to :animal
 
-  # validates :status, presence: true
-  # validates :start_date, presence: true
-  # validates :end_date, presence: true
+  validates :status, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 
 
 end
