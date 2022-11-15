5010aa7653284d90311f25fe6195bc2eb952d653
Will Hargrave willhargrave@hotmail.co.uk <willhargrave@hotmail.co.uk>
2022-11-15 11:16:02 +0900

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :animals
+  resources :bookings, only: [:index, :update]
+  resources :animals, only: [:index, :show, :create] do
+    resources :bookings, only: [:create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
