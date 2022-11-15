58514076585407882be753d93c57b1ee9f86877d
Will Hargrave willhargrave@hotmail.co.uk <willhargrave@hotmail.co.uk>
2022-11-15 16:59:10 +0900

    added dynamic colors for the booking cards

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
   def index
-    @bookings = Booking.all
+    @bookings = current_user.bookings
   end
 
   def create
@@ -15,5 +15,6 @@ class BookingsController < ApplicationController
 
   end
 
+private
 
 end
