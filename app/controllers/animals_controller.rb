13fb49081e9368662fcec596e4693374a21c2370
Will Hargrave willhargrave@hotmail.co.uk <willhargrave@hotmail.co.uk>
2022-11-16 15:04:13 +0900

    added borders for booking cards and link to animal show

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -6,7 +6,8 @@ class AnimalsController < ApplicationController
     @markers = @animals.geocoded.map do |animal|
       {
         lat: animal.latitude,
-        lng: animal.longitude
+        lng: animal.longitude,
+        
       }
     end
   end
